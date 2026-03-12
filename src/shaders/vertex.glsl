4e2ce939d25b7cbb50cf999d3d55110fb7cccd56
CastDev-j <23031429@itcelaya.edu.mx>
2026-03-11 18:33:58 -0600

    refactor: streamline fragment and vertex shaders by removing unused code and optimizing variable usage

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,13 +1,18 @@
 uniform float uTime;
 
 varying vec3 vPosition;
+varying float vOffset;
 
 void main() {
-    vec3 localPosition = position;
+    float offset = sin(position.y * 2.0 + uTime * 3.0) * 0.1;
 
-    float wave = sin(uTime + position.x * 0.5) * cos(uTime + position.z * 0.5) * 0.35;
-    localPosition.y += wave;
+    vec3 localPosition = position;
+    localPosition.x += offset;
+    localPosition.y += sin(position.x * 3.0 + uTime * 4.0) * 0.2;
 
     vPosition = localPosition;
+    vOffset = offset;
+
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
 }
